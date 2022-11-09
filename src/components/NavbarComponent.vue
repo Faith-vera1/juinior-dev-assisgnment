767678eff6c7168f698c5a1bf05626d3e9181a94
Faith-vera1 <ikpantanfaith@gmail.com>
2022-11-09 06:23:32 +0100

    first commit

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="bar">
+    <div id="title">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent",
+  props: ["title"],
+}
+</script>
+
+<style scoped>
+
+  #bar{
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    background-color: orangered;
+    border-bottom: 1pt solid rgb(20, 20, 50);
+
+    height: 8vh;
+    width: 100vw;
+
+  }
+  #title {
+    position: relative;
+    float: left;
+    font-weight: bold;
+    font-size: 4vh;
+
+    color: rgb(50, 50, 114);
+
+    top: 2vh;
+    left: 2vw;
+  }
+
+</style>
